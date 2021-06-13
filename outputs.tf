763af22e18276005d0c5bb38e74fa99b4684dd80
Sean Sundberg <seansund@us.ibm.com>
2021-06-13 11:11:45 -0500

    Fixes name output dependencies
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "name" {
   value       = var.name
   description = "Namespace name"
   depends_on  = [
-    null_resource.copy_cloudnative_configmaps,
-    null_resource.copy_cloudnative_secrets
+    null_resource.setup_argocd,
+    null_resource.setup_namespace
   ]
 }
