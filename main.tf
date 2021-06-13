9efdf89cac02e086d79e89e78b4909eeb28c26ae
Sean Sundberg <seansund@us.ibm.com>
2021-06-13 11:13:47 -0500

    Sets config_namespace value
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 locals {
   config_project = "1-infrastructure"
   application_branch = "main"
+  config_namespace = "default"
 }
 
 resource null_resource setup_namespace {
@@ -16,7 +17,7 @@ resource null_resource setup_namespace {
 resource null_resource setup_argocd {
   depends_on = [null_resource.setup_namespace]
   provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-argocd.sh '${var.config_repo}' '${var.config_paths.infrastructure}' '${local.config_project}' '${var.application_repo}' '${var.application_paths.infrastructure}/namespaces' '${var.namespace}' '${local.application_branch}'"
+    command = "${path.module}/scripts/setup-argocd.sh '${var.config_repo}' '${var.config_paths.infrastructure}' '${local.config_project}' '${var.application_repo}' '${var.application_paths.infrastructure}/namespaces' '${local.config_namespace}' '${local.application_branch}'"
 
     environment = {
       TOKEN = var.config_token
