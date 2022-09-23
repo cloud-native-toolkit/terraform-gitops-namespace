5425b1686a9f2fae69369c5202b64103ed413d0d
Sean Sundberg <seansund@us.ibm.com>
2022-09-23 14:08:11 -0500

    Updates validation logic
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -7,6 +7,12 @@ export KUBECONFIG=$(cat .kubeconfig)
 
 source "${SCRIPT_DIR}/validation-functions.sh"
 
+NAMESPACE=$(jq -r '.name // "gitops-namespace"' gitops-output.json)
+BRANCH=$(jq -r '.branch // "main"' gitops-output.json)
+SERVER_NAME=$(jq -r '.server_name // "default"' gitops-output.json)
+LAYER=$(jq -r '.layer_dir // "2-services"' gitops-output.json)
+TYPE=$(jq -r '.type // "base"' gitops-output.json)
+
 mkdir -p .testrepo
 
 git clone https://${GIT_TOKEN}@${GIT_REPO} .testrepo
@@ -14,12 +20,6 @@ git clone https://${GIT_TOKEN}@${GIT_REPO} .testrepo
 cd .testrepo || exit 1
 
 find . -name "*"
-
-NAMESPACE=$(jq -r '.name // "gitops-namespace"' gitops-output.json)
-BRANCH=$(jq -r '.branch // "main"' gitops-output.json)
-SERVER_NAME=$(jq -r '.server_name // "default"' gitops-output.json)
-LAYER=$(jq -r '.layer_dir // "2-services"' gitops-output.json)
-TYPE=$(jq -r '.type // "base"' gitops-output.json)
 
 validate_gitops_ns_content "${NAMESPACE}"
 
