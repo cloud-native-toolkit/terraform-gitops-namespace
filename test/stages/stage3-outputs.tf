cd6f4ff8faa4ba12956f3cea03bff4af2296e038
Sean Sundberg <seansund@us.ibm.com>
2022-09-23 13:55:11 -0500

    Updates test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -5,7 +5,7 @@ resource local_file write_outputs {
   content = jsonencode({
     name        = module.gitops_namespace.name
     branch      = module.gitops_namespace.branch
-    namespace   = module.gitops_namespace.namespace
+    namespace   = module.gitops_namespace.name
     server_name = module.gitops_namespace.server_name
     layer       = module.gitops_namespace.layer
     layer_dir   = module.gitops_namespace.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_namespace.layer == "services" ? "2-services" : "3-applications")
