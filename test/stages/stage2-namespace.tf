038377bc7ccd08eab3be4553b8b1d10f1c29d325
Sean Sundberg <seansund@us.ibm.com>
2022-09-23 15:27:20 -0500

    Converts to use gitops provider (#45)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-namespace.tf
+++ test/stages/stage2-namespace.tf
@@ -3,6 +3,6 @@ module "gitops_namespace" {
 
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
-  name = var.namespace
+  name = local.test_namespace
   ci = true
 }
