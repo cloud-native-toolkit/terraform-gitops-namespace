9a133c3a4cb537085eaa9cd346d6a17b5260c084
Sean Sundberg <seansund@us.ibm.com>
2022-10-13 22:15:00 -0500

    Updates gitops config to use namespace helm chart
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -1,68 +1,15 @@
 #!/usr/bin/env bash
 
 YAML_DIR="$1"
-NAMESPACE="$2"
-CREATEOG="$3"
-ARGOCD_NAMESPACE="$4"
+CREATEOG="$2"
+ARGOCD_NAMESPACE="$3"
+CI="$4"
 
 mkdir -p "${YAML_DIR}"
 
-cat > "${YAML_DIR}/ns.yaml" <<EOL
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: $NAMESPACE
-  annotations:
-    argocd.argoproj.io/sync-wave: "-30"
+cat > "${YAML_DIR}/values.yaml" <<EOL
+createOperatorGroup: ${CREATEOG}
+argocdNamespace: ${ARGOCD_NAMESPACE}
+gitopsConfig:
+  create: false
 EOL
-
-cat > "${YAML_DIR}/rbac.yaml" <<EOL
-apiVersion: rbac.authorization.k8s.io/v1
-kind: Role
-metadata:
-  name: argocd-admin
-  namespace: ${NAMESPACE}
-  annotations:
-    argocd.argoproj.io/sync-wave: "-20"
-rules:
-- apiGroups:
-  - "*"
-  resources:
-  - "*"
-  verbs:
-  - "*"
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: argocd-admin
-  namespace: ${NAMESPACE}
-  annotations:
-    argocd.argoproj.io/sync-wave: "-20"
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: Role
-  name: argocd-admin
-subjects:
-- apiGroup: rbac.authorization.k8s.io
-  kind: Group
-  name: system:serviceaccounts:${ARGOCD_NAMESPACE}
-EOL
-
-if [[ "${CREATEOG}" == "true" ]]; then
-  cat >> "${YAML_DIR}/ns.yaml" << EOL
----
-apiVersion: operators.coreos.com/v1
-kind: OperatorGroup
-metadata:
-  name: ${NAMESPACE}-operator-group
-  namespace: ${NAMESPACE}
-  annotations:
-    argocd.argoproj.io/sync-wave: "-20"
-spec:
-  targetNamespaces:
-    - ${NAMESPACE}
----
-EOL
-
-fi
