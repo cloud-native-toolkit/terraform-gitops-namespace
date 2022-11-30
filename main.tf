24b9d76e329181df18acb05ab6bb638cae873dd6
Sean Sundberg <seansund@us.ibm.com>
2022-11-30 11:09:56 -0600

    Updates module to use terraform provider
    - Replaces null_resource with gitops provider resource - closes #37
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,53 +1,16 @@
 locals {
-  bin_dir  = module.setup_clis.bin_dir
-  yaml_dir = "${path.cwd}/.tmp/namespace-${var.name}"
+  tmp_dir = ".tmp/namespace-${var.name}"
   create_operator_group = var.name != "openshift-operators" && var.create_operator_group
 }
 
-module setup_clis {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+resource gitops_namespace ns {
+  name = var.name
+  create_operator_group = local.create_operator_group
+  argocd_namespace = var.argocd_namespace
+  dev_namespace = var.ci
+  server_name = var.server_name
+  branch = var.branch
+  config = var.gitops_config
+  credentials = var.git_credentials
+  tmp_dir = local.tmp_dir
 }
-
-resource null_resource create_yaml {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/create-yaml.sh '${local.yaml_dir}' '${local.create_operator_group}' '${var.argocd_namespace}' '${var.ci}'"
-
-    environment = {
-      BIN_DIR = local.bin_dir
-    }
-  }
-}
-
-resource null_resource setup_gitops {
-  depends_on = [null_resource.create_yaml]
-
-  triggers = {
-    name = var.name
-    yaml_dir = local.yaml_dir
-    server_name = var.server_name
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
-    bin_dir = local.bin_dir
-    chart_version = "0.2.0"
-  }
-
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-namespace ${self.triggers.name} --serverName ${self.triggers.server_name} --helmRepoUrl https://charts.cloudnativetoolkit.dev --helmChart namespace --helmChartVersion ${self.triggers.chart_version} --valueFiles ${self.triggers.yaml_dir}/values.yaml"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-namespace ${self.triggers.name} --delete --contentDir ${self.triggers.yaml_dir} --serverName ${self.triggers.server_name}"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-}
-
